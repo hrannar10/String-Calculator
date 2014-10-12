5927b1211721d9bdc1379ed1d59080c60ffe9fbe
Hrannar Már Ágústsson <hrannar@hrannar.net>
2014-10-12 21:35:50 +0000

    inital commit

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest{
+
+	public static void main(String args[]){
+		
+		org.junit.runner.JunitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
+
+	/*@Test
+	public void test(){
+		assertEquals(0, Calculator.add(""));
+	}*/
+}
