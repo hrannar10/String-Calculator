8ada139a356a380d44f1d0ebf384a84cd66575c4
Hrannar Már Ágústsson <hrannar@hrannar.net>
2014-10-12 23:39:47 +0000

    stringMultipleIteam refactored

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,20 +6,25 @@ public class Calculator{
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-			String[] numbers = text.split(",");
-			
-			int theSum = 0;
-			int thelength = numbers.length;
-				
-				for(int i = 0; i < thelength; i++){
-					theSum = theSum + toInt(numbers[i]);
-				}
-			return theSum;
+		else if(text.contains(",")){		
+			return sum(arrNumbers(text));
 		}
 		else{
 			return 1;
 		}
+	}
+	
+	private static String[] arrNumbers(String text){
+		return text.split(",");
+	}
+
+	private static int sum(String[] arrNumbers){
+
+		int theSum = 0;			
+			for(int i = 0; i < arrNumbers.length; i++){
+				theSum += toInt(arrNumbers[i]);
+			}
+		return theSum;
 	}
 
 	private static int toInt(String number){
